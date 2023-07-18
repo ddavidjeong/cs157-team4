dd9c5b2e8ed7ddcc24c187f6133d3c38d9197037
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-07-18 10:43:37 -0700

    Update orderBean.java

--- src/main/java/object/orderBean.java
+++ src/main/java/object/orderBean.java
@@ -1,25 +1,86 @@
 package object;
 
+import java.util.Date;
+
 public class orderBean {
 	private int order_id;
 	private int listing_id;
 	private String tenant_id;
+	private String roomType;
+	private int numPeople;
+	private int price;
+	private String state;
+	Date startDate = new Date();
+	Date endDate = new Date();
+	
+	public String getRoomType() {
+		return roomType;
+	}
+	
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+	
+	public int getNumPeople() {
+		return numPeople;
+	}
+	
+	public void setNumPeople(int numPeople) {
+		this.numPeople = numPeople;
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
 	
 	public int getOrder_id() {
 		return order_id;
 	}
+	
 	public void setOrder_id(int order_id) {
 		this.order_id = order_id;
 	}
+	
 	public int getListing_id() {
 		return listing_id;
 	}
+	
 	public void setListing_id(int listing_id) {
 		this.listing_id = listing_id;
 	}
+	
 	public String getTenant_id() {
 		return tenant_id;
 	}
+	
 	public void setTenant_id(String tenant_id) {
 		this.tenant_id = tenant_id;
 	}
